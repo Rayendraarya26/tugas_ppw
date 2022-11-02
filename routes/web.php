a9e18b94be20a2728ab8aea24296507f862379cf
Rayendraarya26 <88335755+Rayendraarya26@users.noreply.github.com>
2022-11-02 21:18:01 +0700

    perubahan pertemuan 9

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
+use App\Models\Post;
+use App\Mail\SendEmail;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\SendEmailController;
 use App\Http\Controllers\Auth\LoginController;
 
 
@@ -50,3 +56,18 @@ Auth::Routes([
 ]);
 
 Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
+
+Route::get('/send-email', [SendEmailController::class, 'index'])->name('kirim-email');
+Route::post('/post-email', [SendEmailController::class, 'store'])->name('post-email');
+
+// Route::get('/send-email',function(){
+//     $data = [
+//     'name' => 'Nama Anda',
+//     'body' => 'Testing Kirim Email'
+//     ];
+   
+//     Mail::to('rayendraarya7@gmail.com')->send(new SendEmail($data));
+   
+//     dd("Email Berhasil dikirim.");
+//    });
+
