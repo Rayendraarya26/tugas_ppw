9523c3ee17427a15be1adae8a801e932c6cc843f
Rayendraarya26 <88335755+Rayendraarya26@users.noreply.github.com>
2022-12-22 08:32:22 +0700

    Last?

--- app/Http/Controllers/postController.php
+++ app/Http/Controllers/postController.php
@@ -165,7 +165,7 @@ class PostController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+ public function destroy($id)
     {
         $post = Post::find($id);
         if ($post->picture) {
